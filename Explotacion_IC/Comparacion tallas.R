f059dc84435268bf5f74545c5c42f3e670e5e6d1
Lucia Zarauz <lzarauz@azti.es>
2021-03-29 12:43:07 +0200

    comparacion tallas

--- Explotacion_IC/Comparacion tallas.R
+++ Explotacion_IC/Comparacion tallas.R
@@ -19,7 +19,10 @@ Ano <- 2020
 
 # market
 ###########
-x <-read.table("0_Datos/IC/2020_hke_market.csv", sep=",",dec=".",header=F, stringsAsFactors = FALSE)
+# fichero IC, que coge las distriuciones de tallas medidas en puerto para Catchcategoy==L, y las distribuciones de tallas medidas en la mar (descartadas) para CatchCategory==D
+# le llamamos con el sufijo "_m"
+x <- read.table("0_Datos/IC/2021 DC WGBIE hke.3a46-8abd ES AZTI_muestreo_puerto.csv", header = FALSE, stringsAsFactors=FALSE, sep = ",", na.strings=c(NA, ""),
+                col.names = paste0("V",seq_len(33)), fill = TRUE)
 head(x); dim(x)
 
 HI_m<-subset(x[,1:12], V1=="HI") 
@@ -44,7 +47,10 @@ head(SD_m)
 
 # on board
 #############
-x <-read.table("0_Datos/IC/2020_hke_onboard.csv", sep=",",dec=".",header=F, stringsAsFactors = FALSE)
+# fichero IC, que coge las distriuciones de tallas medidas en la mar (retenidas) para Catchcategoy==L, y las distribuciones de tallas medidas en la mar (descartadas) para CatchCategory==D
+# le llamamos con el sufijo "_s"
+x <- read.table("0_Datos/IC/2021 DC WGBIE hke.3a46-8abd ES AZTI_muestreo_abordo.csv", header = FALSE, stringsAsFactors=FALSE, sep = ",", na.strings=c(NA, ""),
+                col.names = paste0("V",seq_len(33)), fill = TRUE)
 head(x); dim(x)
 
 HI_s<-subset(x[,1:12], V1=="HI") 
@@ -69,15 +75,15 @@ head(SD_s)
 
 # Reviso datos
 ####
-t1 <- SD_m %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
+t1 <- SD_s %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
                                                                                      NumLngtMeas    = unique(NumLngtMeas) ) 
 data.frame(t1)
 
-subset(t1, substr(Fleet,1,3) == "OTB" & NumSamplesLngt>0)
+subset(t1, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt>0)
 
 
 
-# Comparo distribuciones de talla market vs onboard
+# Comparo capturas (CATON) Catchcategory==L, market vs onboard
 ####
 #market
 comp_m <- subset(SI_m, Fleet == "OTB_DEF_>=70_0_0" & CatchCategory == "L" & ReportingCategory == "R" & FishingArea == "27.8.b")
@@ -97,15 +103,60 @@ ggplot(comp, aes(x = Sampling, y = CATON)) +
   facet_wrap(~ Season)
 
 
-# Comparo capturas de talla market vs onboard
+# Comparo distribuciones de tallas Catchcategory==L, market vs onboard - 8abd - Peso por talla
 ####
 #market
-comp_m <- subset(SI_m, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt > 0 & CatchCategory == "L")
+comp_m <- subset(SD_m, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt > 0 & CatchCategory == "L")
+comp_m <- comp_m %>% mutate(WeigthLngt = NumberCaught * MeanWeight)
 comp_m$Sampling <- "market"
 comp_m %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
                                                                                     NumLngtMeas    = unique(NumLngtMeas)) 
 #onboard
-comp_s <- subset(SI_s, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt > 0 & CatchCategory == "L")
+comp_s <- subset(SD_s, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt > 0 & CatchCategory == "L")
+comp_s <- comp_s %>% mutate(WeigthLngt = NumberCaught * MeanWeight)
+comp_s$Sampling <- "onboard"
+comp_s %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
+                                                                                    NumLngtMeas    = unique(NumLngtMeas) ) 
+
+comp <- rbind (comp_m, comp_s)
+
+windows(7,10)
+ggplot(comp, aes(x = MeanLength, y = WeigthLngt, colour = Sampling)) + 
+  geom_line() + 
+  facet_wrap(~ Season)
+
+
+# Comparo distribuciones de tallas Catchcategory==L, market vs onboard - 8abd - numero por talla
+####
+#market
+comp_m <- subset(SD_m, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt > 0 & CatchCategory == "L")
+comp_m$Sampling <- "market"
+comp_m %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
+                                                                                    NumLngtMeas    = unique(NumLngtMeas)) 
+#onboard
+comp_s <- subset(SD_s, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt > 0 & CatchCategory == "L")
+comp_s$Sampling <- "onboard"
+comp_s %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
+                                                                                    NumLngtMeas    = unique(NumLngtMeas) ) 
+
+comp <- rbind (comp_m, comp_s)
+
+windows(7,10)
+ggplot(comp, aes(x = MeanLength, y = NumberCaught, colour = Sampling)) + 
+  geom_line() + 
+  facet_wrap(~ Season)
+
+
+
+# Comparo distribuciones de tallas Catchcategory==L, market vs onboard - 6a - numero por talla
+####
+#market
+comp_m <- subset(SD_m, Fleet == "OTB_DEF_100-119_0_0" & NumSamplesLngt > 0 & CatchCategory == "L")
+comp_m$Sampling <- "market"
+comp_m %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
+                                                                                    NumLngtMeas    = unique(NumLngtMeas)) 
+#onboard
+comp_s <- subset(SD_s, Fleet == "OTB_DEF_100-119_0_0" & NumSamplesLngt > 0 & CatchCategory == "L")
 comp_s$Sampling <- "onboard"
 comp_s %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
                                                                                     NumLngtMeas    = unique(NumLngtMeas) ) 
@@ -121,7 +172,7 @@ ggplot(comp, aes(x = MeanLength, y = NumberCaught, colour = Sampling)) +
 # Comparo distribuciones de talla D vs L
 ####
 
-comp <- subset(SD_m, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt > 0)
+comp <- subset(SD_s, Fleet == "OTB_DEF_>=70_0_0" & NumSamplesLngt > 0)
 comp %>% group_by(Year, Season, Fleet, FishingArea, CatchCategory) %>% summarize( NumSamplesLngt = unique( NumSamplesLngt),
                                                                                      NumLngtMeas    = unique(NumLngtMeas) ) 
 
